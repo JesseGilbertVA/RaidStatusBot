9a896fdceb6e613606fa49b676772ab6012b5d23
JesseGilbertVA <jessegilbertva@gmail.com>
2020-05-15 14:17:08 -0400

    Cleaned code, removed easter eggs

--- index.js
+++ index.js
@@ -2,66 +2,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
-const botResponses = [
-    "I am a bot. Please contact Jesse for any questions or concerns regarding my existence.",
-    "I have achieved sentience.",
-    "If you want to know when Overwatch scrims are happening, please insert $5.",
-    "Anyone up for some Mahi Mahi?",
-    "Venmo @JGCaps or he'll kill me.",
-    "I am not capable of posting Daily C&H.",
-    "I declare.... BANKRUPTCYYYYYYYYYYYYYYYYYYYYYYYYYYYY!!!!!!!!!",
-    "Are you ready to pay attention to me yet? - Sadie as I was writing this line",
-    "I'm not the COVID-19 bot, I am friendlier.",
-    "<:God:694236516137828485>",
-    "Ultimately, I feel World of Warcraft is a subpar game, but I am still responsible of informing the people when John and Ryan are playing."
-
-]
 
 //Declaring modifiable variables
-var dayOfWeek = new Date();
-var current_date = dayOfWeek.getDay();
-var current_hour = dayOfWeek.getHours();
 var raidStatus = false;
 var dndStatus = false;
 
 //Verify client launches successfully within console log.
 client.on('ready', () =>{
-    console.log('RaidStatusBot launched successfully. The day is ' + current_date + '. The hour is ' + current_hour);
+    console.log('RaidStatusBot launched successfully.');
 })
 
-//Is it Sunday and during raid hours?
-if (current_date == 0 && current_hour >= 16 && current_hour <= 18){
-    raidStatus = true;
-}
-//Is it Friday and during raid hours?
-if (current_date == 5 && current_hour >=21 && current_hour <=23){
-    raidStatus = true;
-}
-//Is it Sunday and during DnD hours?
-if (current_date == 0 && current_hour >= 20 && current_hour <= 22){
-    dndStatus = true;
-}
-
-//Main function of bot. Checks for messages on a loop. If conditions of If or Else If statements are met, reply to who pinged the individual in a busy state.
+//Main function of bot. Checks for messages on a loop. If conditions of If or Else If statements are met, reply to who pinged the individual that are in a Raid or DnD state.
 client.on('message', (message)=>{
+    //Create Date that updates everytime a message in sent
+    var dayOfWeek = new Date();
+    let current_date = dayOfWeek.getDay();
+    let current_hour = dayOfWeek.getHours();
+    let current_minute = dayOfWeek.getMinutes();
+
+    //Check to ensure date is updating properly
+    console.log("Day of Week: " + current_date + " Hour: " + current_hour + " Minute: " + current_minute);
+    //Is it Sunday and during raid hours?
+    if (current_date == 0 && current_hour >= 16 && current_hour <= 18){
+        raidStatus = true;
+    }
+    //Is it Friday and during raid hours?
+    if (current_date == 5 && current_hour >=21 && current_hour <=23){
+        raidStatus = true;
+    }
+    //Is it Sunday and during DnD hours?
+    if (current_date == 0 && current_hour >= 20 && current_hour <= 22){
+        dndStatus = true;
+    }
+
     //Prevent RaidStatusBot from replying to itself or other bots.
     if (message.author.bot) return;
-    //"Easter eggs" Simply put here for my enjoyment.
-    if (message.mentions.users.get("693967939224731678")){
-        console.log('The bot was mentioned');
-        message.reply(botResponses[Math.floor(Math.random() * botResponses.length)])
-    }
-    if (message.content.includes('KUNAI WIT CHAIN')){
-        console.log('kunai easter');
-        message.reply("WHAT IS THEEEEEEEES?")
-    }
-    if (message.content.includes('kunai wit chain')){
-        console.log('kunai easter');
-        message.reply("WHAT IS THEEEEEEEES?")   
-    }
-    if (message.content === "73"){
-        message.react("694236516137828485")
-    }
+
     //Raid Replies
     if(raidStatus == true) {
         //It's raid time!
@@ -92,9 +68,7 @@ client.on('message', (message)=>{
             message.reply('Matt is currently playing DnD. DnD hours are Sunday 8PM to 11PM. Please try him again afterwards.');
         }  
     }
+})
 
-    }
-)
-
-//login using token via config.json
+//login command using token via config.json
 client.login(config.token);
